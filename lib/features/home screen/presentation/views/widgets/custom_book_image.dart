d2044f8f8985a17d4f0078db7ca4a26870075f26
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-09 12:21:46 +0300

    custom_book_image.dart

--- lib/features/home screen/presentation/views/widgets/custom_book_image.dart
+++ lib/features/home screen/presentation/views/widgets/custom_book_image.dart
@@ -1,27 +1,30 @@
-import 'package:bookly/core/utils/assets_data.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({
     Key? key,
     required this.padding,
+    required this.imageUrl,
   }) : super(key: key);
   final EdgeInsetsGeometry padding;
+  final String imageUrl;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: padding,
       child: AspectRatio(
         aspectRatio: 0.66,
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(14),
-              image: const DecorationImage(
-                fit: BoxFit.fill,
-                image: AssetImage(
-                  AssetsData.testImage,
-                ),
-              )),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(14),
+          child: CachedNetworkImage(
+            imageUrl: imageUrl,
+            fit: BoxFit.fill,
+            errorWidget: (context, url, error) => const Icon(
+              Icons.error_outline_rounded,
+            ),
+          ),
         ),
       ),
     );
