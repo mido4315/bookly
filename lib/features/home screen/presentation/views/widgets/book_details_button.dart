c469002e70718515ec7c7d5d4f474f141b7228b9
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-09 12:20:45 +0300

    book_details_button.dart

--- lib/features/home screen/presentation/views/widgets/book_details_button.dart
+++ lib/features/home screen/presentation/views/widgets/book_details_button.dart
@@ -7,9 +7,9 @@ class BookDetailsButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return const Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: const [
+      children: [
         CustomButton(
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(12),
