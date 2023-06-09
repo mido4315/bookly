52912534db2006434f5fb314206937bfe54f2c7a
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-09 12:20:04 +0300

    best_seller_list_view.dart

--- lib/features/home screen/presentation/views/widgets/best_seller_list_view.dart
+++ lib/features/home screen/presentation/views/widgets/best_seller_list_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../core/utils/widgets/custom_error.dart';
+import '../../../../../core/utils/widgets/custom_loading_indicator.dart';
+import '../../../manger/newest_books_cubit/newest_books_cubit.dart';
 import 'best_seller_list_view_item.dart';
 
 class BestSellerListView extends StatelessWidget {
@@ -7,13 +11,29 @@ class BestSellerListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverList(
-      delegate: SliverChildBuilderDelegate(
-        childCount: 22,
-        (context, index) {
-          return const BestSellerListViewItem();
-        },
-      ),
+    return BlocBuilder<NewestBooksCubit, NewestBooksState>(
+      builder: (context, state) {
+        if (state is NewestBooksSuccess) {
+          return SliverList(
+            delegate: SliverChildBuilderDelegate(
+              childCount: state.newestBooksS.length,
+              (context, index) {
+                return BestSellerListViewItem(
+                  bookModel: state.newestBooksS[index],
+                );
+              },
+            ),
+          );
+        } else if (state is NewestBooksFailure) {
+          return SliverToBoxAdapter(
+            child: CustomErrorMessage(errorMessage: state.errorMessage),
+          );
+        } else {
+          return const SliverToBoxAdapter(
+            child: CustomLoadingIndicator(),
+          );
+        }
+      },
     );
   }
 }
