ba85eda7807612981984edd2f86864c6ea17d0b7
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-10 16:31:44 +0300

    add assets_data.dart

--- lib/core/utils/assets_data.dart
+++ lib/core/utils/assets_data.dart
@@ -1,4 +1,7 @@
+
+
 class AssetsData {
   static const logo = 'assets/images/Logo.png';
   static const testImage = 'assets/images/test_image.jpg';
+  static const imageNotFound = 'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png';
 }
