1058917832b6a43a5c50e145b4e3d35daa3ad4b6
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-10 16:35:11 +0300

    create similar_books_cubit.dart

---
+++ lib/features/home screen/manger/similar_books_cubit/similar_books_cubit.dart
@@ -0,0 +1,29 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../../data/models/book_model/book_model.dart';
+import '../../data/repos/home_repo.dart';
+
+part 'similar_books_state.dart';
+
+class SimilarBooksCubit extends Cubit<SimilarBooksState> {
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchSimilarBooks(String category) async {
+    emit(SimilarBooksLoading());
+    var result = await homeRepo.fetchSimilarBooks(category);
+    result.fold(
+      (failure) => emit(
+        SimilarBooksFailure(
+          failure.errorMessage,
+        ),
+      ),
+      (similarBooks) => emit(
+        SimilarBooksSuccess(
+          similarBooks,
+        ),
+      ),
+    );
+  }
+}
