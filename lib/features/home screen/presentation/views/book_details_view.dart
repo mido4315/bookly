3f6290dcc27e8a035271cf0d6c8b54d81f3b535f
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-10 16:33:24 +0300

    handle bloc builder

--- lib/features/home screen/presentation/views/book_details_view.dart
+++ lib/features/home screen/presentation/views/book_details_view.dart
@@ -1,13 +1,18 @@
 import 'package:bookly/features/home%20screen/presentation/views/widgets/book_details_view_body.dart';
 import 'package:flutter/material.dart';
 
+import '../../data/models/book_model/book_model.dart';
+
 class BookDetailsView extends StatelessWidget {
-  const BookDetailsView({Key? key}) : super(key: key);
+  const BookDetailsView({Key? key, required this.bookModel}) : super(key: key);
+  final BookModel bookModel;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: BookDetailsViewBody(),
+    return Scaffold(
+      body: BookDetailsViewBody(
+        bookModel: bookModel,
+      ),
     );
   }
 }
