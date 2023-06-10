4bda19e390545d8dda147a062ea3dcb8240dad7a
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-10 16:34:10 +0300

    link custom app bar

--- lib/features/home screen/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home screen/presentation/views/widgets/custom_app_bar.dart
@@ -1,7 +1,7 @@
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:bookly/core/utils/assets_data.dart';
 import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
