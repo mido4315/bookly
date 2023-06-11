668da5c4f474eaea3ed1933a69150b4dd3fa2d75
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-12 02:50:59 +0300

    update home_repo_impl.dart and add similar books function

--- lib/features/home screen/data/repos/home_repo.dart
+++ lib/features/home screen/data/repos/home_repo.dart
@@ -1,8 +1,11 @@
 import 'package:dartz/dartz.dart';
+
 import '../../../../core/errors/failure.dart';
 import '../models/book_model/book_model.dart';
 
 abstract class HomeRepo{
  Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
  Future<Either<Failure,List<BookModel>>> fetchNewestBooks();
+ Future<Either<Failure,List<BookModel>>> fetchSimilarBooks(String category);
+
 }
