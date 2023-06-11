538581d743cda593c9d847b3af4e86b1b4b214cc
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-12 02:47:20 +0300

    create service_locator.dart

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,16 +1,18 @@
+import 'package:bookly/features/search%20screen/presentation/data/repos/search_repo_impl.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+
 import '../../features/home screen/data/repos/home_repo_impl.dart';
 import 'api_service.dart';
 
 final getIt = GetIt.instance;
 
-void setup() {
-  getIt.registerSingleton<HomeRepoImpl>(
-    HomeRepoImpl(
-      APIService(
-        Dio(),
-      ),
-    ),
-  );
+void setupServiceLocator() {
+  getIt.registerSingleton<APIService>(APIService(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(
+    getIt.get<APIService>(),
+  ));
+  getIt.registerSingleton<SearchRepoImpl>(SearchRepoImpl(
+    getIt.get<APIService>(),
+  ));
 }
