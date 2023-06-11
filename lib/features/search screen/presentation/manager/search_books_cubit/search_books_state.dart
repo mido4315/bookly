845351f0662cc20889ffe76ed00370d3f7cd79a2
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-12 02:48:13 +0300

    create search_books_state.dart

---
+++ lib/features/search screen/presentation/manager/search_books_cubit/search_books_state.dart
@@ -0,0 +1,15 @@
+part of 'search_books_cubit.dart';
+
+@immutable
+abstract class SearchBooksState {}
+
+class SearchBooksInitial extends SearchBooksState {}
+class SearchBooksLoading extends SearchBooksState {}
+class SearchBooksSuccess extends SearchBooksState {
+  SearchBooksSuccess(this.searchBooks);
+  final List<BookModel> searchBooks;
+}
+class SearchBooksFailure extends SearchBooksState {
+  SearchBooksFailure(this.errorMessage);
+  final String errorMessage;
+}
