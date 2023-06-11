8e3ced82ced85e6e79395087b050489c914c0b6d
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-12 02:46:59 +0300

    update custom_search_bar.dart

--- lib/features/search screen/presentation/views/widgets/custom_search_bar.dart
+++ lib/features/search screen/presentation/views/widgets/custom_search_bar.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../manager/search_books_cubit/search_books_cubit.dart';
 
 class CustomSearchBar extends StatelessWidget {
   const CustomSearchBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SliverToBoxAdapter(
+    return SliverToBoxAdapter(
       child: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: TextField(
-          decoration: InputDecoration(
+          // onSubmitted: (value) {
+          //   BlocProvider.of<SearchBooksCubit>(context).fetchSearchResult(value);
+          // },
+          onChanged: (value) {
+            if (value.trim() != '') {
+              BlocProvider.of<SearchBooksCubit>(context).badRequest = false;
+              BlocProvider.of<SearchBooksCubit>(context)
+                  .fetchSearchResult(value);
+            } else {
+              BlocProvider.of<SearchBooksCubit>(context).badRequest = true;
+            }
+          },
+          decoration: const InputDecoration(
             border: OutlineInputBorder(),
             hintText: 'Search',
             suffixIcon: Icon(
