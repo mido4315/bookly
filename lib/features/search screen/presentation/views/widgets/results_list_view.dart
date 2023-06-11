35ccf55133b52901022772fc570ed46db17dbc9e
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-12 02:48:55 +0300

    update results_list_view.dart

--- lib/features/search screen/presentation/views/widgets/results_list_view.dart
+++ lib/features/search screen/presentation/views/widgets/results_list_view.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../core/utils/widgets/custom_error.dart';
+import '../../../../../core/utils/widgets/custom_loading_indicator.dart';
 import '../../../../home screen/presentation/views/widgets/best_seller_list_view_item.dart';
+import '../../manager/search_books_cubit/search_books_cubit.dart';
 
 class ResultsListView extends StatelessWidget {
   const ResultsListView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SliverList(
-      delegate: SliverChildBuilderDelegate(
-        childCount: 22,
-        (context, index) {
-         // return const BestSellerListViewItem();
-        },
-      ),
+    return BlocBuilder<SearchBooksCubit, SearchBooksState>(
+      builder: (context, state) {
+        if (state is SearchBooksSuccess) {
+          if (BlocProvider.of<SearchBooksCubit>(context).badRequest == false) {
+            return SliverList(
+              delegate: SliverChildBuilderDelegate(
+                childCount: state.searchBooks.length,
+                    (context, index) {
+                  return NewestListViewItem(
+                    bookModel: state.searchBooks[index],
+                  );
+                },
+              ),
+            );
+          }else{
+            return const SliverToBoxAdapter(
+              child: CustomLoadingIndicator(),
+            );
+          }
+        } else if (state is SearchBooksFailure) {
+          return SliverToBoxAdapter(
+            child: CustomErrorMessage(errorMessage: state.errorMessage),
+          );
+        } else {
+          return const SliverToBoxAdapter(
+            child: CustomLoadingIndicator(),
+          );
+        }
+      },
     );
   }
 }
