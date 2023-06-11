ab2a8afd7b95a2a8ebefbeacfbd2cd0c30770237
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-12 02:46:51 +0300

    update custom_loading_indicator.dart

--- lib/core/utils/widgets/custom_loading_indicator.dart
+++ lib/core/utils/widgets/custom_loading_indicator.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CustomLoadingIndicator extends StatelessWidget {
   const CustomLoadingIndicator({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: CircularProgressIndicator(),
+    return  Center(
+      child: Shimmer.fromColors(
+        baseColor: Colors.deepPurple,
+        highlightColor: Colors.blueAccent,
+        child: const Text(
+          'Loading',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 40.0,
+            fontWeight:
+            FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
