961e7db9f6ade1ea7e9c7d512a668cc61d40761e
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-12 02:48:21 +0300

    create search_repo.dart

---
+++ lib/features/search screen/presentation/data/repos/search_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../../core/errors/failure.dart';
+import '../../../../home screen/data/models/book_model/book_model.dart';
+
+
+abstract class SearchRepo{
+  Future<Either<Failure,List<BookModel>>> fetchSearchResult(String searchString);
+}
