f699b44b25d95dc30c149b323b32fdc099f9fed2
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-23 03:48:14 +0300

    error handle widget

--- lib/core/utils/widgets/custom_error.dart
+++ lib/core/utils/widgets/custom_error.dart
@@ -2,10 +2,14 @@ import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class CustomErrorMessage extends StatelessWidget {
-  const CustomErrorMessage({Key? key, required this.errorMessage}) : super(key: key);
-final String errorMessage;
+  const CustomErrorMessage({Key? key, required this.errorMessage})
+      : super(key: key);
+  final String errorMessage;
   @override
   Widget build(BuildContext context) {
-    return Text(errorMessage,style: Styles.textStyle20,);
+    return Text(
+      errorMessage,
+      style: Styles.textStyle20,
+    );
   }
 }
